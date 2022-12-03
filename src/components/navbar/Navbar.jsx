c9de92f275a5dae18c132b200a82eba9add2c92b
Franssen <ngoiefranssen04@gmail.com>
2022-12-03 11:01:35 +0200

    Wip

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,55 +3,66 @@ const Navbar = () =>{
 
     return(
         <div>
-            <nav className="navbar navbar-expand-lg nav-fixed bg-light bg-white py-4 shadow-sm">
-            <div className="container">
-                <a className="navbar-brand fw-bold fs-4" href="/">
-                TransMoney
-                </a>
-                {/* <buthrefn
-                className="navbar-hrefggler"
-                type="buthrefn"
-                data-bs-hrefggle="collapse"
-                data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="hrefggle navigation"
+      <nav className="navbar navbar-expand-lg bg-light bg-white py-4 shadow-sm">
+        <div className="container">
+          <Link className="navbar-brand fw-bold fs-4" to="/">
+            TransMoney
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link
+                  className="nav-Link active"
+                  aria-current="page"
+                  to="/"
                 >
-                <span className="navbar-hrefggler-icon"></span>
-                </buthrefn> */}
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mx-auhref mb-2 mb-lg-0">
-                    <li className="nav-item">
-                    <a
-                        className="nav-a active"
-                        aria-current="page"
-                        href="/"
-                    >
-                        Home
-                    </a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-a" href="/products">
-                        ListDepot
-                    </a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-a" href="/about">
-                        ListTransfert
-                    </a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-a" href="/contact">
-                        Contact
-                    </a>
-                    </li>
-                </ul>
-                </div>
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-Link" to="/listdepot">
+                  ListsDepot
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-Link" to="/about">
+                  ListTransfert
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-Link" to="/contact">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+            <div className="buttons">
+              <Link className="btn btn-outline-dark">
+                <i className="fa fa-sign-in me-1" to="/login"></i> Login
+              </Link>
+              <Link className="btn btn-outline-success ms-2">
+                <i className="fa fa-user-plus me-2 ms" to="/sing_up"></i>
+                Sing-Up
+              </Link>
+              <Link className="btn btn-outline-dark ms-2">
+                <i className="fa fa-profil me-1" to="/cart"></i> Cart(
+                {state.length});
+              </Link>
             </div>
-            </nav>
-
-            
-        </div>   
+          </div>
+        </div>
+      </nav>
+    </div>  
     );
 };
 
